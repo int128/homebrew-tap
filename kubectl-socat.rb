a0a05444a401c27260caa24399fcdecf0900f73e
Hidetake Iwata <int128@gmail.com>
2021-03-09 10:05:18 +0900

    Update kubectl-socat to v0.2.0

--- kubectl-socat.rb
+++ kubectl-socat.rb
@@ -1,15 +1,15 @@
 class KubectlSocat < Formula
   desc "kubectl plugin to connect to external host via Kubernetes cluster"
   homepage "https://github.com/int128/kubectl-socat"
-  version "v0.1.0"
+  version "v0.2.0"
 
   on_macos do
-    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_v0.1.0_darwin_amd64.zip"
-    sha256 "295b650da9766fc4e7ccc80201223f2366b800c419cc3bfab804a2d6e87f4cc0"
+    url "https://github.com/int128/kubectl-socat/releases/download/v0.2.0/kubectl-socat_v0.2.0_darwin_amd64.zip"
+    sha256 "2809248e54d655cf13ddc896df51cb0b1e187a9193f2e7a310f75299b3821250"
   end
   on_linux do
-    url "https://github.com/int128/kubectl-socat/releases/download/v0.1.0/kubectl-socat_v0.1.0_linux_amd64.zip"
-    sha256 "a0862211648202ba80e3c33078ab1c8c2821dc6419e5fa8c496e83500cbfcd34"
+    url "https://github.com/int128/kubectl-socat/releases/download/v0.2.0/kubectl-socat_v0.2.0_linux_amd64.zip"
+    sha256 "f121fbfcbc01d893767d7fd31c7f4b62bb1db729ba0cd8eab2f46abec90ffa8e"
   end
 
   def install
