fb631e0fe514035dc95e249d1f5418c618ef6971
Hidetake Iwata <int128@gmail.com>
2021-04-14 17:58:35 +0900

    Update kubectl-external-forward.rb

--- kubectl-external-forward.rb
+++ kubectl-external-forward.rb
@@ -1,15 +1,15 @@
 class KubectlExternalForward < Formula
   desc "kubectl plugin to connect to external host via Kubernetes cluster"
   homepage "https://github.com/int128/kubectl-external-forward"
-  version "v0.4.0"
+  version "v0.5.0"
 
   on_macos do
-    url "https://github.com/int128/kubectl-external-forward/releases/download/v0.4.0/kubectl-external_forward_darwin_amd64.zip"
-    sha256 "91bb5ebef6a143574754ad11d6254d21170e3693005751b79698f3978434b6ea"
+    url "https://github.com/int128/kubectl-external-forward/releases/download/v0.5.0/kubectl-external_forward_darwin_amd64.zip"
+    sha256 "7fc7ff441a81b0bb6c652ec935a4c3acfb28751dda0510562bcc4808b891a0d6"
   end
   on_linux do
-    url "https://github.com/int128/kubectl-external-forward/releases/download/v0.4.0/kubectl-external_forward_linux_amd64.zip"
-    sha256 "ecbda3adf9fb35043a672afe65b381690263e1f3e58c0b6c9cfd38c5e3fac818"
+    url "https://github.com/int128/kubectl-external-forward/releases/download/v0.5.0/kubectl-external_forward_linux_amd64.zip"
+    sha256 "8877036ef63408a09537cf61672073b06c144d1a2d6e4552b3f19887cd311105"
   end
 
   def install
